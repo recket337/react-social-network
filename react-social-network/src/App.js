4a2e3531764d237268487badd0075eb57ab8a76d
Ilya Kuzin <gign.brest@gmail.com>
2021-07-15 02:22:06 +0300

    feat: add initial components

---
+++ react-social-network/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import { Header } from './components/Header';
+import { Nav } from './components/Nav';
+import { Main } from './components/Main';
+
+function App() {
+  return (
+    <div className="app-wrapper">
+      
+      <Header />
+      
+      <Nav />
+
+      <Main />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ react-social-network/src/components/Nav.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export const Nav = () => {
+    return (
+        <nav className="navbar">
+        <ul>
+          <li>
+            <a>Profile</a>
+          </li>
+          <li>
+            <a>Messages</a>
+          </li>
+          <li>
+            <a>News</a>
+          </li>
+          <li>
+            <a>Music</a>
+          </li>
+          <li>
+            <a>Settings</a>
+          </li>
+        </ul>
+      </nav>
+    )
+}
