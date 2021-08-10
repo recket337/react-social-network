68ea7604ab2c86f8bbab5a52bb680db6a0d7bffe
Ilya Kuzin <gign.brest@gmail.com>
2021-08-10 03:11:47 +0300

    feat: upload users from API

--- react-social-network/src/components/Users/Users.jsx
+++ react-social-network/src/components/Users/Users.jsx
@@ -1,55 +1,72 @@
 import React from "react";
 import s from "./Users.module.css";
+import * as axios from "axios";
+import userImage from "../../assets/images/user.jpg";
 
 export const Users = (props) => {
-  if(props.users.length === 0) {
-    props.setUsers([
-      {
-        id: 1,
-        photoUrl:
-          "https://lumiere-a.akamaihd.net/v1/images/spiderman-characterthumbnail-spiderman_3a64e546.jpeg?region=0%2C0%2C300%2C300",
-        followed: false,
-        fullName: "Sasha",
-        status: "fe",
-        location: { city: "Minsk", country: "Belarus" },
-      },
-      {
-        id: 2,
-        photoUrl:
-          "https://lumiere-a.akamaihd.net/v1/images/spiderman-characterthumbnail-spiderman_3a64e546.jpeg?region=0%2C0%2C300%2C300",
-        followed: true,
-        fullName: "Dmitry",
-        status: "fe",
-        location: { city: "Moscow", country: "Russia" },
-      },
-      {
-        id: 3,
-        photoUrl:
-          "https://lumiere-a.akamaihd.net/v1/images/spiderman-characterthumbnail-spiderman_3a64e546.jpeg?region=0%2C0%2C300%2C300",
-        followed: false,
-        fullName: "Anton",
-        status: "fe",
-        location: { city: "Kiev", country: "Ukraine" },
-      },
-      {
-        id: 4,
-        photoUrl:
-          "https://lumiere-a.akamaihd.net/v1/images/spiderman-characterthumbnail-spiderman_3a64e546.jpeg?region=0%2C0%2C300%2C300",
-        followed: false,
-        fullName: "Andrew",
-        status: "fe",
-        location: { city: "Biala Podlaska", country: "Poland" },
-      },
-    ]);
+
+  const getUsers = () => {
+    if (props.users.length === 0) {
+      axios
+        .get("https://social-network.samuraijs.com/api/1.0/users")
+        .then((response) => {
+          props.setUsers(
+            response.data.items
+            /* [
+        {
+          id: 1,
+          photoUrl:
+            "https://lumiere-a.akamaihd.net/v1/images/spiderman-characterthumbnail-spiderman_3a64e546.jpeg?region=0%2C0%2C300%2C300",
+          followed: false,
+          fullName: "Sasha",
+          status: "fe",
+          location: { city: "Minsk", country: "Belarus" },
+        },
+        {
+          id: 2,
+          photoUrl:
+            "https://lumiere-a.akamaihd.net/v1/images/spiderman-characterthumbnail-spiderman_3a64e546.jpeg?region=0%2C0%2C300%2C300",
+          followed: true,
+          fullName: "Dmitry",
+          status: "fe",
+          location: { city: "Moscow", country: "Russia" },
+        },
+        {
+          id: 3,
+          photoUrl:
+            "https://lumiere-a.akamaihd.net/v1/images/spiderman-characterthumbnail-spiderman_3a64e546.jpeg?region=0%2C0%2C300%2C300",
+          followed: false,
+          fullName: "Anton",
+          status: "fe",
+          location: { city: "Kiev", country: "Ukraine" },
+        },
+        {
+          id: 4,
+          photoUrl:
+            "https://lumiere-a.akamaihd.net/v1/images/spiderman-characterthumbnail-spiderman_3a64e546.jpeg?region=0%2C0%2C300%2C300",
+          followed: false,
+          fullName: "Andrew",
+          status: "fe",
+          location: { city: "Biala Podlaska", country: "Poland" },
+        },
+        ] */
+          );
+        });
+    }
   }
+
 
   return (
     <div>
+      <button onClick={getUsers}>Get users</button>
       {props.users.map((u) => (
         <div key={u.id}>
           <span>
             <div>
-              <img src={u.photoUrl} className={s.avatar} />
+              <img
+                src={u.photos.small != null ? u.photos.small : userImage}
+                className={s.avatar}
+              />
             </div>
             <div>
               {u.followed ? (
@@ -73,12 +90,12 @@ export const Users = (props) => {
           </span>
           <span>
             <span>
-              <div>{u.fullName}</div>
+              <div>{u.name}</div>
               <div>{u.status}</div>
             </span>
             <span>
-              <div>{u.location.country}</div>
-              <div>{u.location.city}</div>
+              <div>{"u.location.country"}</div>
+              <div>{"u.location.city"}</div>
             </span>
           </span>
         </div>
