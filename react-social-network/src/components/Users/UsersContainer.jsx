694eac71ebebfd1979afa78d0e308f94b5187fcd
Ilya Kuzin <gign.brest@gmail.com>
2021-08-11 02:51:21 +0300

    refactor: API requests moved into container component

--- react-social-network/src/components/Users/UsersContainer.jsx
+++ react-social-network/src/components/Users/UsersContainer.jsx
@@ -1,7 +1,44 @@
 import React from "react";
 import { connect } from "react-redux";
 import { followAC, setUsersAC, unfollowAC, setCurrentPageAC, setUsersTotalCountAC } from "../../redux/users-reducer";
-import Users from "../Users/Users"
+import { Users } from "./Users";
+import * as axios from "axios";
+
+export class UsersAPIComponent extends React.Component {
+  componentDidMount() {
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+        this.props.setTotalUsersCount(response.data.totalCount);
+        this.props.setTotalUsersCount(100); // HARDCODE
+      });
+  }
+
+  onPageChanged = (pageNumber) => {
+    this.props.setCurrentPage(pageNumber);
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+      });
+  };
+
+  render() {
+    return  <Users totalUsersCount={this.props.totalUsersCount}
+        pageSize={this.props.pageSize}
+        currentPage={this.props.currentPage}
+        onPageChanged={this.onPageChanged}
+        users={this.props.users}
+        follow={this.props.follow}
+        unfollow={this.props.unfollow}
+    />
+  }
+}
 
 const mapStateToProps = (state) => {
   return {
@@ -32,4 +69,4 @@ const mapDispatchToProps = (dispatch) => {
   }
 }
 
-export const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users)
+export const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersAPIComponent)
